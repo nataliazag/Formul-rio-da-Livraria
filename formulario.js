8eb65b6f25d691d89e9eb87c7657d1e5b8c5dcd5
Natália Zaguini <162721549+nataliazag@users.noreply.github.com>
2024-07-08 12:43:35 -0300

    Add files via upload
    SUBINDO

---
+++ formulario.js
@@ -0,0 +1,23 @@
+function Enviar(){
+let nomeUsuario = document.getElementById('name').value
+let emailUsuario = document.getElementById('email').value
+let cpfUsuario = document.getElementById('number').value
+let dataReserva = document.getElementById('data').value
+
+let lugar = document.querySelector("input[name='lugar']:checked").value
+
+alert('Seu nome ' + nomeUsuario + '\n' 
++ 'Seu email é: ' +emailUsuario+ '\n' 
++ 'Seu CPF é: ' +cpfUsuario+ '\n'
++ 'A data da sua reserva foi: ' +dataReserva+ '\n'
++ 'Retirada: ' + lugar)
+
+window.location.href='ultima.html'
+
+
+}
+
+function Voltar(){
+window.location.href='formulario.html'
+
+}
