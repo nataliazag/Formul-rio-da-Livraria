1544a13dc4436d917873457cbc61194d0b506513
Natália Zaguini <162721549+nataliazag@users.noreply.github.com>
2024-07-11 09:47:30 -0300

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -6,18 +6,28 @@ let dataReserva = document.getElementById('data').value
 
 let lugar = document.querySelector("input[name='lugar']:checked").value
 
+let termoDeUso = document.getElementById('termoDeUso')
+
+if(termoDeUso.checked){
+    alert('Tudo certo! Veja suas informações a seguir ')
+    window.location.href='ultima.html'
+}else{
+    alert('Ahh, acho que você esqueceu de confirmar os termos!')
+}
+
 alert('Seu nome ' + nomeUsuario + '\n' 
 + 'Seu email é: ' +emailUsuario+ '\n' 
 + 'Seu CPF é: ' +cpfUsuario+ '\n'
 + 'A data da sua reserva foi: ' +dataReserva+ '\n'
 + 'Retirada: ' + lugar)
 
-window.location.href='ultima.html'
+
+
 
 
 }
 
 function Voltar(){
-window.location.href='formulario.html'
+window.location.href='formulario.html                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            '
 
 }
